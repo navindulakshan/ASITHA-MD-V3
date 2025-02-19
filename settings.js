05f854f46a58fcb06c644467310e2f5791d353d2
navindulakshan <dew1234dew12345679@gmail.com>
2025-02-19 17:41:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     
-SESSION_ID: process.env.SESSION_ID === undefined ? 'මේක කපලා ඔයාලගේ Session id එක දාන්න' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASITHA-MD=e7JGOYBJ#Ulwqh5dTEn5ICKILX-JLOKOS3QcruRnbz5TWBP8tb9w' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94787072548,94704227534' : process.env.SUDO,
